537d1da0bb3b6e78a83e24ebb5d3f14d40cb58e9
Ze das Couves <murilohenriquemendes99@gmail.com>
2026-09-15 13:41:43 -0300

    Palavras palindromas

---
+++ palindromo.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+bool is_palindrome(std::string text){
+  std::string name = text;
+  std::string r_name = text;
+  bool result = true;
+  
+  for(int i = 0; i < text.size(); ++i){
+    for(int j = text.size() - 1; j >=0; --j){
+        if(name[i] == r_name[j]){
+            result = true;
+            i += 1;
+        } else {
+            result = false;
+            j = -1;
+            break;
+        }
+    if(result == false){
+        break;
+    }
+    }
+  }
+  return result;
+}
+
+int main() {
+  
+  std::cout << std::boolalpha << is_palindrome("madam") << "\n";
+  std::cout << std::boolalpha << is_palindrome("ada") << "\n";
+  std::cout << std::boolalpha << is_palindrome("lovelace") << "\n";
+
+  std::cin.ignore(1000, '\n');
+  std::cout << "Tecle <Enter> para encerrar";
+  std::cin.get();
+
+  return 0;
+}
